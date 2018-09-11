e3594b649ea54ecafe88ebea02747b3804372aaa
Veronika Loukotova <VeronikaL@kentico.com>
2018-09-11 16:22:07 +0200

    KA-354 - Create ListController + test

---
+++ KenticoOnboardingApplication/KenticoOnboardingApplication.Api.Tests/ListControllerTest.cs
@@ -0,0 +1,46 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Configuration;
+using KenticoOnboardingApplication.Api.Controllers;
+using NUnit.Framework;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Results;
+
+namespace KenticoOnboardingApplication.Api.Tests
+{
+    [TestFixture]
+    public class ListControllerTest
+    {
+        private ListController controller;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var config = new HttpConfiguration();
+            config.Routes.MapHttpRoute("TestApi", "testTemplate");
+
+            controller = new ListController()
+            {
+                Configuration = config,
+                Request = new System.Net.Http.HttpRequestMessage()
+            };
+        }
+
+        [Test]
+        public async Task GetAllItems()
+        {
+            var expectedResult = controller.items;
+
+            var result = await controller.Get();
+            var executedResult = await result.ExecuteAsync(CancellationToken.None);
+            string[] resultValue;
+            executedResult.TryGetContentValue(out resultValue);
+
+            Assert.AreEqual(executedResult.StatusCode, HttpStatusCode.OK);
+            Assert.AreEqual(resultValue, expectedResult);
+        }
+    }
+}
---
+++ KenticoOnboardingApplication/KenticoOnboardingApplication.Api/Controllers/ListController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using NUnit.Framework.Constraints;
+
+namespace KenticoOnboardingApplication.Api.Controllers
+{
+    public class ListController : ApiController
+    {
+        public readonly string[] items =
+        {
+            "Learn C#",
+            "Create dummy controller",
+            "Connect JS and TS"
+        };
+
+        // GET: api/List
+        public async Task<IHttpActionResult> Get()
+        {
+            return await Task.FromResult<IHttpActionResult>(Ok(items));
+        }
+
+        // GET: api/List/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/List
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/List/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/List/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
