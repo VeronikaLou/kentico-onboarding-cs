7f48d27681df113ee34dcac33a686cae3397aa53
Veronika Loukotova <VeronikaL@kentico.com>
2018-09-18 16:49:10 +0200

    KA-354 - Add JsonConfig to App_Start

--- KenticoOnboardingApplication/KenticoOnboardingApplication.Api/App_Start/WebApiConfig.cs
+++ KenticoOnboardingApplication/KenticoOnboardingApplication.Api/App_Start/WebApiConfig.cs
@@ -1,23 +1,22 @@
 ﻿using System.Web.Http;
+using System.Web.Http.Routing;
+using Microsoft.Web.Http.Routing;
 
 namespace KenticoOnboardingApplication.Api
 {
     public static class WebApiConfig
     {
-        internal static readonly string RouteName = "ApiV1";
-
         public static void Register(HttpConfiguration config)
         {
-            // Web API configuration and services
+            var constraintResolver = new DefaultInlineConstraintResolver
+            {
+                ConstraintMap =
+                {
+                    ["apiVersion"] = typeof( ApiVersionRouteConstraint )
+                }
+            };
 
-            // Web API routes
-            config.MapHttpAttributeRoutes();
-
-            config.Routes.MapHttpRoute(
-                name: RouteName,
-                routeTemplate: "api/v1/{controller}/{id}",
-                defaults: new {controller = "List", id = RouteParameter.Optional}
-            );
+            config.MapHttpAttributeRoutes(constraintResolver);
         }
     }
 }
