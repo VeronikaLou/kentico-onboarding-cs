960f81a6d845dfc6d4683a30ef910393dc98e4d0
Veronika Loukotova <VeronikaL@kentico.com>
2018-09-18 16:49:45 +0200

    KA-354 - Edit Post in ListController

--- KenticoOnboardingApplication/KenticoOnboardingApplication.Api/Controllers/ListController.cs
+++ KenticoOnboardingApplication/KenticoOnboardingApplication.Api/Controllers/ListController.cs
@@ -2,13 +2,17 @@
 using System.Net;
 using System.Threading.Tasks;
 using System.Web.Http;
-using System.Web.Http.Routing;
 using KenticoOnboardingApplication.Api.Models;
+using Microsoft.Web.Http;
 
 namespace KenticoOnboardingApplication.Api.Controllers
 {
+    [ApiVersion("1.0")]
+    [RoutePrefix("api/v{version:apiVersion}/List")]
     public class ListController : ApiController
     {
+        public ListController() { }
+
         private static readonly Item[] Items =
         {
             new Item {Text = "Learn C#"},
@@ -16,24 +20,27 @@ namespace KenticoOnboardingApplication.Api.Controllers
             new Item {Text = "Connect JS and TS"}
         };
 
+        [Route]
         public async Task<IHttpActionResult> GetAllItems() =>
             await Task.FromResult(Ok(Items));
 
+        [Route("{id:guid}", Name = "Get")]
         public async Task<IHttpActionResult> GetItem(Guid id) =>
             await Task.FromResult(Ok(Items[0]));
 
+        [Route]
         public async Task<IHttpActionResult> PostItem([FromBody] Item value)
         {
-            var guid = new {id = "d95f4249-6f37-46ab-b102-b55972306910"};
-            var url = new UrlHelper(Request).Route(WebApiConfig.RouteName, guid);
-            var uri = new Uri(url, UriKind.Relative);
+            var uri = Url.Link("Get", new {id = "d95f4249-6f37-46ab-b102-b55972306910"});
 
             return await Task.FromResult(Created(uri, Items[1]));
         }
 
+        [Route("{id:guid}")]
         public async Task<IHttpActionResult> PutItem(Guid id, [FromBody] Item value) =>
             await Task.FromResult(Ok(Items[0]));
 
+        [Route("{id:guid}")]
         public async Task<IHttpActionResult> DeleteItem(Guid id) =>
             await Task.FromResult(StatusCode(HttpStatusCode.NoContent));
     }
