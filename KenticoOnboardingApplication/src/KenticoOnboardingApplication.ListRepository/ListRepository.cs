ac6a45b1a482b071f15789cc90f18155b9166d63
Veronika Loukotova <VeronikaL@kentico.com>
2018-09-19 12:28:03 +0200

    KA-355 - Implement ListRepository

--- KenticoOnboardingApplication/src/KenticoOnboardingApplication.ListRepository/ListRepository.cs
+++ KenticoOnboardingApplication/src/KenticoOnboardingApplication.ListRepository/ListRepository.cs
@@ -8,27 +8,22 @@ namespace KenticoOnboardingApplication.ListRepository
 {
     public class ListRepository : IListRepository
     {
-        public Task<IHttpActionResult> GetAllItems()
+        private static readonly Item[] Items =
         {
-            throw new NotImplementedException();
-        }
+            new Item {Id = new Guid("00000000-0000-0000-0000-000000000001"), Text = "Learn C#"},
+            new Item {Id = new Guid("00000000-0000-0000-0000-000000000002"), Text = "Create dummy controller"},
+            new Item {Id = new Guid("00000000-0000-0000-0000-000000000003"), Text = "Connect JS and TS"}
+        };
 
-        public Task<IHttpActionResult> GetItem(Guid id)
-        {
-            throw new NotImplementedException();
-        }
+        public async Task<Item[]> GetAllItems() => await Task.FromResult(Items);
 
-        public Task<IHttpActionResult> PostItem(Item item)
-        {
-            throw new NotImplementedException();
-        }
+        public async Task<Item> GetItem(Guid id) => await Task.FromResult(Items[0]);
 
-        public Task<IHttpActionResult> PutItem(Guid id, Item item)
-        {
-            throw new NotImplementedException();
-        }
+        public async Task<Item> PostItem(Item item) => await Task.FromResult(Items[1]);
 
-        public Task<IHttpActionResult> DeleteItem(Guid id)
+        public async Task<Item> PutItem(Guid id, Item item) => await Task.FromResult(Items[0]);
+
+        public async Task DeleteItem(Guid id)
         {
             throw new NotImplementedException();
         }
