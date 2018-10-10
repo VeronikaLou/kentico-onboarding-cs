0b904f2bcfd20d7ac2f0f026e53de1e85cd1d99d
Veronika Loukotova <VeronikaL@kentico.com>
2018-10-10 15:49:52 +0200

    KA-356 - Add properties LastUpdate, Creation to Item

--- KenticoOnboardingApplication/src/KenticoOnboardingApplication.Contracts/Models/Item.cs
+++ KenticoOnboardingApplication/src/KenticoOnboardingApplication.Contracts/Models/Item.cs
@@ -6,6 +6,8 @@ namespace KenticoOnboardingApplication.Contracts.Models
     {
         public Guid Id { get; set; }
         public string Text { get; set; }
+        public DateTime LastUpdate { get; set; } = DateTime.Now;
+        public DateTime Creation { get; set; } = DateTime.Now;
 
         public override string ToString() => $"Item {Id} = {Text}";
     }
