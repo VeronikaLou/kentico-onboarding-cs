1472656a7e0e6957b6d99cae14313700c221de6d
Veronika Loukotova <VeronikaL@kentico.com>
2018-10-10 09:49:14 +0200

    KA-355 - Edit UnityResolver
    Create ResolveServiceType function, s_caughtExceptions field, call ResolveServiceType from GetService, GetServices.

--- KenticoOnboardingApplication/src/KenticoOnboardingApplication.Api/Resolvers/UnityResolver.cs
+++ KenticoOnboardingApplication/src/KenticoOnboardingApplication.Api/Resolvers/UnityResolver.cs
@@ -1,8 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Web.Http.Controllers;
 using System.Web.Http.Dependencies;
+using System.Web.Http.Description;
+using System.Web.Http.Dispatcher;
+using System.Web.Http.ExceptionHandling;
+using System.Web.Http.Hosting;
 using Unity;
 using Unity.Exceptions;
+using Unity.Resolution;
 
 namespace KenticoOnboardingApplication.Api.Resolvers
 {
@@ -10,32 +18,25 @@ namespace KenticoOnboardingApplication.Api.Resolvers
     {
         private readonly IUnityContainer _container;
 
+        private static readonly List<string> s_caughtExceptions = new List<string>()
+        {
+            nameof(IHostBufferPolicySelector),
+            nameof(IHttpControllerSelector),
+            nameof(IHttpControllerActivator),
+            nameof(IHttpActionSelector),
+            nameof(IHttpActionInvoker),
+            nameof(IContentNegotiator),
+            nameof(IExceptionHandler)
+        };
+
         public UnityResolver(IUnityContainer container) =>
             _container = container ?? throw new ArgumentNullException(nameof(container));
 
-        public object GetService(Type serviceType)
-        {
-            try
-            {
-                return _container.Resolve(serviceType);
-            }
-            catch (ResolutionFailedException)
-            {
-                return null;
-            }
-        }
+        public object GetService(Type serviceType) =>
+            ResolveServiceType(() => _container.Resolve(serviceType), null);
 
-        public IEnumerable<object> GetServices(Type serviceType)
-        {
-            try
-            {
-                return _container.ResolveAll(serviceType);
-            }
-            catch (ResolutionFailedException)
-            {
-                return new List<object>();
-            }
-        }
+        public IEnumerable<object> GetServices(Type serviceType) =>
+            ResolveServiceType(() => _container.ResolveAll(serviceType), Enumerable.Empty<object>());
 
         public IDependencyScope BeginScope()
         {
@@ -44,5 +45,17 @@ namespace KenticoOnboardingApplication.Api.Resolvers
         }
 
         public void Dispose() => _container.Dispose();
+
+        private static T ResolveServiceType<T>(Func<T> resolve, T returnValue)
+        {
+            try
+            {
+                return resolve();
+            }
+            catch (ResolutionFailedException ex) when (s_caughtExceptions.Contains(ex.TypeRequested))
+            {
+                return returnValue;
+            }
+        }
     }
 }
