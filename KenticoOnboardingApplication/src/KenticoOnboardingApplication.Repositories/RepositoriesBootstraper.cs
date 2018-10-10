6dc3bc219919537a0643ec4fd31a01cb91fcca72
Veronika Loukotova <VeronikaL@kentico.com>
2018-10-10 10:48:11 +0200

    KA-356 - Configure connection strings
    Inject connection string in RepositoriesBootstraper.

--- KenticoOnboardingApplication/src/KenticoOnboardingApplication.Repositories/RepositoriesBootstraper.cs
+++ KenticoOnboardingApplication/src/KenticoOnboardingApplication.Repositories/RepositoriesBootstraper.cs
@@ -1,6 +1,8 @@
-﻿using KenticoOnboardingApplication.Contracts;
+﻿using System.Configuration;
+using KenticoOnboardingApplication.Contracts;
 using KenticoOnboardingApplication.Contracts.Repositories;
 using Unity;
+using Unity.Injection;
 using Unity.Lifetime;
 
 namespace KenticoOnboardingApplication.Repositories
@@ -8,6 +10,15 @@ namespace KenticoOnboardingApplication.Repositories
     public class RepositoriesBootstraper : IBootstrapper
     {
         public IUnityContainer Register(IUnityContainer container) =>
-            container.RegisterType<IListRepository, Repositories.ListRepository>(new HierarchicalLifetimeManager());
+            container.RegisterType<IListRepository, Repositories.ListRepository>(
+                new HierarchicalLifetimeManager(),
+                InjectConnectionString()
+            );
+
+        private static InjectionConstructor InjectConnectionString()
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["TODOList"].ConnectionString;
+            return new InjectionConstructor(connectionString);
+        }
     }
 }
